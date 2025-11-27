6fc8e48be38e52188038c82c52ba25ea1c6cada7
Gamzat Mamaev <mamaevvgamzatt@gmail.com>
2025-11-27 13:56:53 +0300

    added unit tests and GitHub Actions

---
+++ tests/test_validation.py
@@ -0,0 +1,13 @@
+import pytest
+from main import validate_url_client_side
+
+@pytest.mark.parametrize("url, expected", [
+    ("https://ria.ru", True),
+    ("http://lenta.ru", True),
+    ("https://tass.ru/news", True),
+    ("ftp://invalid.ru", False),
+    ("not_a_url", False),
+    ("https://www.ria.ru", True),
+])
+def test_url_validation(url, expected):
+    assert validate_url_client_side(url) == expected
